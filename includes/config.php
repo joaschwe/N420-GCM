2f395b0d26ce726d3cfa70578bcbb0783ba00137
Joanna Schweiger <joaschwe@iupui.edu>
2018-10-22 06:58:56 -0400

    revised images

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,9 @@
 <?php
 ini_set('display_errors', E_ALL);
 
-define("ABSOLUTE_PATH", "/home/students/joaschwe/public_html/N420-Multimedia-Proj-Dev/N420-GCM/");
-define("URL_ROOT", "https://in-info-web4.informatics.iupui.edu/~joaschwe/N420-Multimedia-Proj-Dev/N420-GCM/");
+//added paths for files to reference to
+define('ABSOLUTE_PATH', '/home/joaschwe/htdocs/CIT313/SP2018/a1-phpDebug');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:21791/CIT313/SP2018/a1-phpDebug');
 
 
 //database info
