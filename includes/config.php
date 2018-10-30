bcce32ab48646fe4b39c84c16b9ffde86419244a
joaschwe <joaschwe@iupui.edu>
2018-10-30 04:09:25 -0400

    added images

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('display_errors', E_ALL);
+
+define("ABSOLUTE_PATH", "/home/students/joaschwe/public_html/N420-Multimedia-Proj-Dev/N420-GCM/");
+define("URL_ROOT", "https://in-info-web4.informatics.iupui.edu/~joaschwe/N420-Multimedia-Proj-Dev/N420-GCM/");
+
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'joaschwe');
+define('DB_PASS', 'joaschwe');
+define('DB_NAME', 'joaschwe_db');
+?>
---
+++ includes/footer.inc.php
@@ -0,0 +1,15 @@
+<hr>
+<div class="foot">
+  &copy; Copyright Joanna Schweiger 2018
+</div>
+
+</div>
+
+
+<script src="lib/jquery-3.1.1.min.js"></script>
+<!--<script src="lib/sweetalert.min.js"></script>-->
+<!--<script src="app/app.js"></script>-->
+
+
+</body>
+</html>
