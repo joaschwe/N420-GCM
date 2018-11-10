23199492c9058d2367697f6e7696770b4e8f020b
joaschwe <joaschwe@iupui.edu>
2018-11-09 22:23:44 -0500

    updating

--- index2.php
+++ index2.php
@@ -7,7 +7,6 @@ require './includes/nav.inc.php';
 ?>
 
 
-
 <div class="container">
 
 
@@ -29,182 +28,212 @@ require './includes/nav.inc.php';
         <div class="card">
             <img src="images/logos/good.png">
             <div class="card-content">
-            <h2 class="clicker">Good</h2>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                laborum.</p>
+                <h2 class="clicker">Good</h2>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
             </div>
         </div>
         <div class="card">
             <img src="images/logos/cause.jpg">
             <div class="card-content">
                 <h2 class="clicker">Cause</h2>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                    laborum.</p>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
             </div>
         </div>
         <div class="card">
             <img src="images/logos/market.jpg">
             <div class="card-content">
                 <h2 class="clicker">Market</h2>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                    laborum.</p>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
             </div>
         </div>
     </div>
 
 
     <!-- PRODUCTS -->
-<!--    <div class="products">-->
-        <h1 class="clicker">Products</h1>
+    <!--    <div class="products">-->
+    <h1 class="clicker">Products</h1>
 
-        <!--GALLERY-->
-        <div class="card-container">
-            <div class="card">
-                <img src="images/products/prodLand/product1.png">
-                <div class="card-content">
+    <!--GALLERY-->
+    <div class="card-container">
+        <div class="card">
+            <img src="images/products/prodLand/product1.png">
+            <div class="card-content">
                 <h4>Excepteur Sint Proident</h4>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                    est laborum.</p>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
                 <p class="price">$0.00</p>
-                <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
             </div>
-            <div class="card">
-                <img src="images/products/prodLand/product2.png">
-                <div class="card-content">
-                    <h4>Excepteur Sint Proident</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.</p>
-                    <p class="price">$0.00</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images/products/prodLand/product3.png">
-                <div class="card-content">
-                    <h4>Excepteur Sint Proident</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.</p>
-                    <p class="price">$0.00</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images/products/prodLand/product4.png">
-                <div class="card-content">
-                    <h4>Excepteur Sint Proident</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.</p>
-                    <p class="price">$0.00</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images/products/prodLand/product5.png">
-                <div class="card-content">
-                    <h4>Excepteur Sint Proident</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.</p>
-                    <p class="price">$0.00</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images/products/prodLand/product6.png">
-                <div class="card-content">
-                    <h4>Excepteur Sint Proident</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.</p>
-                    <p class="price">$0.00</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
-            </div><div class="card">
-                <img src="images/products/prodLand/product7.png">
-                <div class="card-content">
-                    <h4>Excepteur Sint Proident</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.</p>
-                    <p class="price">$0.00</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
-            </div><div class="card">
-                <img src="images/products/prodLand/product8.png">
-                <div class="card-content">
-                    <h4>Excepteur Sint Proident</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.</p>
-                    <p class="price">$0.00</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
-            </div>
-            <p class="more"><a href="good_products.php">More &#8594;</a></p>
         </div>
+        <div class="card">
+            <img src="images/products/prodLand/product2.png">
+            <div class="card-content">
+                <h4>Excepteur Sint Proident</h4>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p class="price">$0.00</p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="images/products/prodLand/product3.png">
+            <div class="card-content">
+                <h4>Excepteur Sint Proident</h4>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p class="price">$0.00</p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="images/products/prodLand/product4.png">
+            <div class="card-content">
+                <h4>Excepteur Sint Proident</h4>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p class="price">$0.00</p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="images/products/prodLand/product5.png">
+            <div class="card-content">
+                <h4>Excepteur Sint Proident</h4>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p class="price">$0.00</p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="images/products/prodLand/product6.png">
+            <div class="card-content">
+                <h4>Excepteur Sint Proident</h4>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p class="price">$0.00</p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="images/products/prodLand/product7.png">
+            <div class="card-content">
+                <h4>Excepteur Sint Proident</h4>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p class="price">$0.00</p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="images/products/prodLand/product8.png">
+            <div class="card-content">
+                <h4>Excepteur Sint Proident</h4>
+                <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p class="price">$0.00</p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+            </div>
+        </div>
+        <p class="more"><a href="good_products.php">See More <span>Products</span></a></p>
+    </div>
 
 
-<!--    </div>-->
+    <!--    </div>-->
 
     <!--CRAFTSMAN-->
-<!--    <div class="craftsman">-->
-        <h1 class="clicker">Craftsman</h1>
+    <!--    <div class="craftsman">-->
+    <h1 class="clicker">Craftsman</h1>
 
-        <div class="card-container">
-            <div class="card">
-                <img src="images/craftsman/craftLand/craftsman1.png">
-                <div class="card-content">
+    <div class="card-container">
+        <div class="card">
+            <img src="images/craftsman/craftLand/craftsman1.png">
+            <div class="card-content">
                 <h3>Excepteur Sint</h3>
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
                     pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                    anim id est laborum.</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
+                    anim id est laborum.
+                </p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
             </div>
-            <div class="card">
-                <img src="images/craftsman/craftLand/craftsman2.jpg">
-                <div class="card-content">
-                <h3>Excepteur Sint</h3>
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                    anim id est laborum.</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images/craftsman/craftLand/craftsman3.png">
-                <div class="card-content">
-                <h3>Excepteur Sint</h3>
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                    anim id est laborum.</p>
-                    <a href="#"><img class="share" src="images/icons/share1.jpg" /></a>
-                </div>
-            </div>
-            <p class="more"><a href="good_craftsmen.php">More &#8594;</a></p>
         </div>
+        <div class="card">
+            <img src="images/craftsman/craftLand/craftsman2.jpg">
+            <div class="card-content">
+                <h3>Excepteur Sint</h3>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                    anim id est laborum.
+                </p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="images/craftsman/craftLand/craftsman3.png">
+            <div class="card-content">
+                <h3>Excepteur Sint</h3>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                    anim id est laborum.
+                </p>
+                <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+            </div>
+        </div>
+        <p class="more"><a href="good_craftsmen.php">More &#8594;</a></p>
+    </div>
 
 
-<!--    </div>-->
+    <!--    </div>-->
 
 
     <!--CAUSES-->
-<!--    <div class="causeSection">-->
-        <h1 class="clicker"><span>Good</span> Causes</h1>
-        <div class="card-container">
-            <div class="card">
-                <img src="images/causes/cause1.png">
-            </div>
-            <div class="card">
-                <img src="images/causes/cause2.png">
-            </div>
-            <div class="card">
-                <img src="images/causes/cause3.png">
-            </div>
-            <div class="card">
-                <img src="images/causes/cause4.png">
-            </div>
-            <p class="more"><a href="good_causes.php">More &#8594;</a></p>
+    <!--    <div class="causeSection">-->
+    <h1 class="clicker"><span>Good</span> Causes</h1>
+    <div class="card-container">
+        <div class="card">
+            <img src="images/causes/cause1.png">
         </div>
+        <div class="card">
+            <img src="images/causes/cause2.png">
+        </div>
+        <div class="card">
+            <img src="images/causes/cause3.png">
+        </div>
+        <div class="card">
+            <img src="images/causes/cause4.png">
+        </div>
+        <p class="more"><a href="good_causes.php">More &#8594;</a></p>
+    </div>
 
-<!--    </div>-->
+    <!--    </div>-->
 
     <!--FOOTER-->
     <?php
