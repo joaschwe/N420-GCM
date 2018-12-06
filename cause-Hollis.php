b4c6e5a9358d0beb5428426157a3941c87a74276
Jeff Strother <jas47167@gmail.com>
2018-12-06 14:28:13 -0500

    Add Hollis Adams cause

---
+++ cause-Hollis.php
@@ -0,0 +1,148 @@
+    <?php
+require './includes/header.inc.php';
+require './includes/nav.inc.php';
+?>
+
+
+<div class="container">
+
+    <div class="craftman" style="margin-bottom:50px;">
+
+        <!--about content-->
+        <div class="craftmanContent">
+
+            <!--about image-->
+            <div class="flex-craft">
+                <div class="craft-img">
+                    <img src="images/causes/hollisAdams.PNG">
+                    <div class="social-media">
+                        <i class="fa-globe"></i>
+                        <i class="fa-twitter"></i>
+                        <i class="fa-facebook"></i>
+                        <i class="fa-instagram"></i>
+                    </div>
+                </div>
+
+
+                <div class="maintitle-craft">
+                    <h3 class="clicker-sansSerif">Hollis Adams</h3>
+<!--                    <h3 class="craft-subhead">sub-header</h3>-->
+                    <h4 class="location">Indiana | a <span>Good</span> Craftsman since 2018</h4>
+                </div>
+            </div>
+            <div class="quote-section">
+
+                <p>
+                    <img src="images/icons/quote-left.png" style="margin-left:-35px; padding-right:5px;">
+                    <b>What is Hollis Adams?</b> <br /> Our mission is to open the world to adults with developmental disabilities in central Indiana.
+                    Hollis Adams (HA) is a privately funded nonprofit that has served adults with developmental disabilities for 50 years. We offer social, recreational, and community-based opportunities through a wide variety of events and activities.
+                    <br/><br/>
+
+                    <b>The Legacy of Hollis Adams</b> <br />
+                    In 1969, parents of individuals with developmental disabilities founded Hollis Adams (HA) to meet the evolving needs of children aging out of the educational system. Originally called Activities of Daily Living (ADL), the primary focus of Hollis Adams was learning and practicing basic skills with an emphasis on increasing independence. Participants learned to cook, clean, and follow a schedule in a sheltered environment surrounded by peers and parents. As a result of the close-knit group of parents and children, many lasting relationships were formed and the vision of Hollis Adams’ founders began to expand.
+                    In 1985, the Foundation was named a beneficiary of the estate of Rose Marie Ellis. The gift was in honor of her friend, Margaret Healey, who was active with the Foundation. With this wonderful and unexpected gift, the Board renewed their commitment to serve adults with developmental disabilities, but chose to focus their efforts on quality social and recreational activities. Initially, most of the social activities involved one-time outings to special events such as concerts, plays, and short driving trips to nearby towns and landmarks. The Board ultimately decided to offer ongoing programs, starting with square dancing lessons and water aerobics. Thanks to the dedication and creativity of our staff, HA programs realized incredible growth. Now we have expanded beyond evening activities to include HAFriends: four days of engaging outings, skill building activities, volunteerism opportunities, and social recreation each week. HA is proud to be a family environment, focused on providing a place for our friends to establish and grow lasting, meaningful relationships and contribute to our local community. Each day of HAFriends includes both pre-planned events and impromptu activities and trips chosen by our participants. HAFriends now functions as a safe, loving platform for interacting with friends, improving social and practical skills, and developing healthy routines. Currently, Hollis Adams evening activities and HAFriends are making a positive impact in the lives of over 250 participants, and our numbers are increasing each month.
+                    <br/><br/>
+
+                    <b>Quick Stats Of Our Progress</b> <br />
+                    Spread the word! The more people who find out about a project, the more support it will get. Please consider posting a link to our website on your Facebook page or joining our Facebook Group, and using our Amazon Smile page to ask Amazon to donate a portion of your next Amazon purchase to Hollis-Adams Foundation. Our programs, participants, and staff rely on the support of our community - you!
+
+                <img src="images/icons/quote-right.png" style="margin-bottom: -18px; margin-left: 5px;">
+                </p>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="causeAmt" style="margin-bottom: 100px;">
+        <h3 class="amt"><span class="raised">$0.00</span> has been raised for this <span>Good</span> Cause</h3>
+    </div>
+
+<div class="sort" style="height: 45px;">
+    <p style="width:100%; font-weight: bold;">More Causes</p>
+
+</div>
+
+    <div class="xs">
+        <div class="card-container">
+            <div class="card">
+                <img src="images/causes/causeHome/boysgirlsclub.png">
+                <div class="card-content">
+                    <a href="cause.php"><h3>Excepteur Sint</h3></a>
+                    <p>
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                    <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/causes/causeHome/childrens.jpg">
+                <div class="card-content">
+                    <a href="cause.php"><h3>Excepteur Sint</h3></a>
+                    <p>
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                    <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/causes/causeHome/coworker.jpg">
+                <div class="card-content">
+                    <a href="cause.php"><h3>Excepteur Sint</h3></a>
+                    <p>
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                    <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/causes/causeHome/red.png">
+                <div class="card-content">
+                    <a href="cause.php"><h3>Excepteur Sint</h3></a>
+                    <p>
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                    <a href="#"><img class="share" src="images/icons/share1.jpg"/></a>
+                </div>
+            </div>
+        </div>
+        <p class="more"><a href="good_causes.php">See All <span>Causes</span></a></p>
+
+    </div>
+
+
+
+
+<!--FOOTER-->
+<?php
+include './includes/footer.inc.php'
+?>
+</div><!--end container-->
+
+<!--
+h3.title {
+font-family: serif;
+font-size: 42px;
+font-weight: normal;
+text-align: left;
+width:100%;
+margin-bottom:10px;
+color:#6b6b6b;
+}
+h3.title span {
+color: #8DC63F;
+font-style: italic;
+text-decoration: none;
+font-family: "ClickerScript-Regular", serif;
+font-size: 54px;
+text-align: left;
+/*padding-left:10px;*/
+}
+-->
