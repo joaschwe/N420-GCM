77c88fe6f7b6ab81cd0940884e941126fb5f5a9e
Jeff Strother <jeff@strotherit.com>
2018-12-06 21:18:00 -0500

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -15,20 +15,13 @@ require './includes/nav.inc.php';
             <h3 class="clicker-sansSerif">About <span>Good Cause Market</span></h3>
             </div>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Accumsan sit amet nulla facilisi morbi tempus iaculis urna. In nibh mauris cursus
-                mattis molestie.<br/><br/>
-
-                Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Congue nisi vitae suscipit
-                tellus. Erat nam at lectus urna. Dictum non consectetur a erat nam at lectus. Phasellus egestas tellus
-                rutrum tellus. Sit amet venenatis urna cursus eget nunc scelerisque viverra. Ornare suspendisse sed nisi
-                lacus sed viverra tellus in.<br/><br/>
-
-                Varius morbi enim nunc faucibus a pellentesque. Risus feugiat in ante metus. Lorem sed risus ultricies
-                tristique nulla aliquet enim. Sed libero enim sed faucibus turpis in eu. Mi tempus imperdiet nulla
-                malesuada pellentesque elit. Donec enim diam vulputate ut pharetra sit amet aliquam id. Faucibus ornare
-                suspendisse sed nisi lacus. Dolor sit amet consectetur adipiscing elit. Hac habitasse platea dictumst
-                vestibulum rhoncus est pellentesque elit.
+                Beablery Distribution, LLC, DBA Good Cause Market, is an e-store that raises funds on a year round basis targeting the beneficiaries, family and friends of a wide group of non-profit organizations. Good Cause Market also has an advantage  in that it provides a target customer base for small, independent businesses. 
+<br /><br />
+The exceptional products that Good Cause Market offers, will satisfy the consumer's desire to not only purchase unique, out of the ordinary, hand-crafted, unconventional products, but also give back to a charitable entity of their choice, at any time, throughout the year.  Good Cause Market (as a gateway service for these organizations, their clientele, customers and the community) allows them to make purchases knowing a portion of it will go to their charity.  There will be no "window of time to purchase" and no door to door selling.
+<br /><br />
+Good Cause Market gives charitable organizations such as schools, church youth groups, boys and girls clubs, sports teams, school clubs and other not for profit organizations that find their budgets in need of other sources, a continuous income stream through its e-tail platform.   These non-profit groups will accomplish this through the purchase of goods from small, independent artisans, craftspeople and etsy-like companies .  
+<br /><br />
+Good Cause Market  has a four-fold uniqueness and advantageous aspect that is unlike any other fundraising company. It provides funding sources for charities.  It provides this funding on a year-round basis. It allows the consumer to purchase exceptional quality items.  It allows the non-profits clients to give back through that purchase. Lastly, it provides a steady, revenue source for small business owners. Good Cause Market will be able to incrementally broaden and increase their market reach by the nature of the focused community/client targeted audience. 
             </p>
 
         </div>
