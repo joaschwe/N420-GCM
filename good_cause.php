05f765c64b5d1d89c37dc6816e5d9ab554fe71ea
Jeff Strother <jeff@strotherit.com>
2019-03-04 05:36:47 -0500

    Update good_cause.php
    Trying to get causes list to load dynamically

--- good_cause.php
+++ good_cause.php
@@ -1,7 +1,12 @@
 <?php
-//require './includes/config.php';
+include("./includes/db_connect.php");
+include("./includes/dropdowns.php");
+
 require './includes/header.inc.php';
 require './includes/nav.inc.php';
+
+
+
 
 //echo $_SERVER['SCRIPT_FILENAME'];
 ?>
@@ -12,12 +17,32 @@ require './includes/nav.inc.php';
     <h1 class="clicker" style="margin-bottom:50px;">Choose a <span>Good</span> Cause</h1>
 
         <form id="example" action="" style="margin:auto; width:70%; border: 1px solid grey; display:flex;box-sizing: border-box; background-color: white; border-radius: 3px;">
-            <input id="searchInput" type="text" placeholder="Boys & Girls Club" name="search2" style="width:80%; margin:0 auto 0 auto; padding: 15px; font-size: 20px; outline:none; border:none;">
+            <select name="cause" id="causeSelection" required>
+            <?php
+            while ($row = mysqli_fetch_array($causeResult, MYSQLI_BOTH)) {
+                echo '<option id="cause' . $row["ID"] . '" value="' . $row["ID"] . '">' . $row["CauseName"] . '</option>';
+//               echo '<input id="cause' . $row["ID"] . '" value="' . $row["ID"] . '" type="text" placeholder="'. $row["CauseName"] .'" name="search2" style="width:80%; margin:0 auto; padding: 15px; font-size: 20px; outline:none; border:none;">';
+
+            }
+            ?>
+            </select>
+
+
+<!--            <input id="searchInput" type="text" placeholder="Boys & Girls Club" name="search2" style="width:80%; margin:0 auto 0 auto; padding: 15px; font-size: 20px; outline:none; border:none;">-->
             <p id="searchBtn" style="margin:5px 0 5px 0; width:40%; font-size:20px; color:#6b6b6b; float:right; text-align: right;">
                 Expires 12/05/2018
+<!--                --><?php
+//                    if (substr( $causeExpirationResult["EndDate"], 0, 4) == '9999') {
+//                        echo "Ongoing";
+//                    } elseif (substr($row["EndDate"], 0, 4) == '0000') {
+//                        echo "Ongoing";
+//                    } else {
+//                        echo $causeExpirationResult . $row["EndDate"];
+//                    }
+//                ?>
             </p>
-                <i class="fa-angle-down fa-3x" id="searchCaret" style="font-family: 'FontAwesome', sans-serif; font-style: normal; margin:3px 15px 0 0; color:#8DC63F; float:right;">
-                </i>
+<!--            <i class="fa-angle-down fa-3x" id="searchCaret" style="font-family: 'FontAwesome', sans-serif; font-style: normal; margin:3px 15px 0 0; color:#8DC63F; float:right;">-->
+<!--            </i>-->
         </form>
 
         <div class="two-across">
